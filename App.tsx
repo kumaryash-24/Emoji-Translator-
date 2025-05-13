972ce4dd5bc7ae22a2a5b0fbf689bd761b3aa334
Kumar Yash <143955711+kumaryash-24@users.noreply.github.com>
2025-05-13 10:19:08 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'; 
 import { Header } from './components/Header';   
 import { TranslatorCard } from './components/TranslatorCard';        
-import { HistoryList } from './components/HistoryList';        
+import { HistoryList } from './components/HistoryList';          
 import { TranslationProvider } from './context/TranslationContext';           
 import { Github } from 'lucide-react';             
   
