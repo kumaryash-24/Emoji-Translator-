6873d9ff0b9f232420ea9de62e30a93416392433
Kumar Yash <143955711+kumaryash-24@users.noreply.github.com>
2025-05-13 10:18:58 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'; 
 import { Header } from './components/Header';   
 import { TranslatorCard } from './components/TranslatorCard';        
 import { HistoryList } from './components/HistoryList';        
